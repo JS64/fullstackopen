ba8fe5e68443af05393bda0fc188731bac73bf42
JS64 <66859474+JS64@users.noreply.github.com>
2020-06-27 06:35:29 +1000

    completed part3 exercise 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', function (req) { 
@@ -8,6 +9,7 @@ morgan.token('body', function (req) {
     }
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
