1bd29a4b78eae51e5770baff9a2f1a366de09627
JS64 <66859474+JS64@users.noreply.github.com>
2020-07-02 00:23:06 +1000

    completed part3 exercise 3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,10 +5,10 @@ const cors = require('cors')
 const app = express()
 const Person = require('./models/person')
 
-morgan.token('body', function (req) { 
-    if (req.method === 'POST') {
-        return JSON.stringify(req.body) 
-    }
+morgan.token('body', function (req) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
 })
 
 app.use(cors())
@@ -17,94 +17,92 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (req, res) => {
-    Person.countDocuments({})
-        .then(total => {
-            res.send(`<p>Phonebook has info for ${total} people.</p><p>${new Date().toString()}</p>`)
-        })
+  Person.countDocuments({})
+    .then(total => {
+      res.send(`<p>Phonebook has info for ${total} people.</p><p>${new Date().toString()}</p>`)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
-    Person.find({})
-        .then(persons => persons.map(person => person.toJSON()))
-        .then(formattedPersons => {
-            res.json(formattedPersons)
-        })
+  Person.find({})
+    .then(persons => persons.map(person => person.toJSON()))
+    .then(formattedPersons => {
+      res.json(formattedPersons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(person.toJSON())
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
- 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        date: new Date(),
-    })
+  const body = req.body
 
-    person.save()
-        .then(savedPerson => savedPerson.toJSON())
-        .then(savedAndFormattedPerson => {
-            res.json(savedAndFormattedPerson)
-        })
-        .catch(error => next(error))
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+  })
+
+  person.save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      res.json(savedAndFormattedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(res.status(204).end())
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
-  
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-    
-    const opts = { new: true, runValidators: true, context: 'query' }
+  const body = req.body
 
-    Person.findByIdAndUpdate(req.params.id, person, opts)
-        .then(updatedPerson => updatedPerson.toJSON())
-        .then(updatedAndFormattedPerson => {
-            res.json(updatedAndFormattedPerson)
-        })
-        .catch(error => next(error))
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  const opts = { new: true, runValidators: true, context: 'query' }
+
+  Person.findByIdAndUpdate(req.params.id, person, opts)
+    .then(updatedPerson => updatedPerson.toJSON())
+    .then(updatedAndFormattedPerson => {
+      res.json(updatedAndFormattedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError' && error.kind == 'ObjectId') {
-        return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
-    next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -10,32 +10,30 @@ const url = process.env.MONGODB_URI
 console.log('Connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('Connected to MongoDB')
-  })
+  .then(console.log('Connected to MongoDB'))
   .catch((error) => {
     console.log('Error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-        unique: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true
-    }, 
-    date: {
-        type: Date,
-        required: true
-    }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  }
 })
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
