e34689f1939447e0ea8136f340a64fbae1748e86
JS64 <66859474+JS64@users.noreply.github.com>
2020-07-03 08:27:37 +1000

    completed part4 exercises 4.5, 4.6 & 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -13,7 +15,43 @@ const totalLikes = (blogs) => {
     : likes.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  const index = likes.indexOf(Math.max(...likes))
+  return blogs.length === 0
+    ? blogs
+    : _.pick(blogs[index], ['title', 'author', 'likes'])
+}
+
+const mostBlogs = (blogs) => {
+  const top = _.chain(blogs)
+    .countBy('author')
+    .map((value, key) => ({ author: key, blogs: value }))
+    .maxBy('blogs')
+    .value()
+  return blogs.length === 0
+    ? blogs
+    : top
+}
+
+const mostLikes = (blogs) => {
+  const top = _.chain(blogs)
+    .groupBy('author')
+    .map((value, key) => ({
+      author: key,
+      likes: value.map(value => value.likes).reduce((sum, item) => sum + item)
+    }))
+    .maxBy('likes')
+    .value()
+  return blogs.length === 0
+    ? blogs
+    : top
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
