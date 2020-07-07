ca8e303ba1fbc0d4db3d69a46bc3f91df15da61a
JS64 <66859474+JS64@users.noreply.github.com>
2020-07-08 09:37:33 +1000

    completed part4 exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 require('express-async-errors')
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -21,8 +22,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
