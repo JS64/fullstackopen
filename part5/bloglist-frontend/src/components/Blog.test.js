06c325e12511419cc6bcf1ee2ba9b874366e69d0
JS64 <66859474+JS64@users.noreply.github.com>
2020-07-15 08:05:07 +1000

    completed part5 exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,28 +1,40 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('Renders only title and author by default', () => {
-  const testUser = {
-    username: 'test',
-    name: 'Test User'
-  }
+const testUser = {
+  username: 'test',
+  name: 'Test User'
+}
 
-  const testBlog = {
-    author: 'Martin Fowler',
-    title: 'Testing Blog Posts',
-    likes: 7,
-    url: 'https://www.google.com',
-    user: testUser
-  }
+const testBlog = {
+  author: 'Martin Fowler',
+  title: 'Testing Blog Posts',
+  likes: 7,
+  url: 'https://www.google.com',
+  user: testUser
+}
 
-  const component = render(
-    <Blog blog={testBlog} user={testUser}/>
-  )
+describe('<Blog />', () => {
+  let component
+  beforeEach(() => {
+    component = render(
+      <Blog blog={testBlog} user={testUser}/>
+    )
+  })
+  test('Renders only title and author by default', () => {
+    expect(component.getByText('Testing Blog Posts Martin Fowler')).toBeVisible()
+    expect(component.getByText('Likes 7')).not.toBeVisible()
+    expect(component.getByText('https://www.google.com')).not.toBeVisible()
+  })
 
-  expect(component.getByText('Testing Blog Posts Martin Fowler')).toBeVisible()
-  expect(component.getByText('Likes 7')).not.toBeVisible()
-  expect(component.getByText('https://www.google.com')).not.toBeVisible()
+  test('Renders title and author after clicking Show', () => {
+    const button = component.getByText('Show')
+    fireEvent.click(button)
 
+    expect(component.getByText('Testing Blog Posts Martin Fowler')).toBeVisible()
+    expect(component.getByText('Likes 7')).toBeVisible()
+    expect(component.getByText('https://www.google.com')).toBeVisible()
+  })
 })
